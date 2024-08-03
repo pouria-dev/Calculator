a965eaff2596a5ac9ad793f64019370bd2f18e36
Piuria <pouriahajipour06@gmail.com>
2024-08-04 00:46:44 +0330

    run tkinter window

---
+++ main.py
@@ -0,0 +1,36 @@
+import tkinter as tk 
+
+calculation = ""
+
+def add_to_calculation(symbol):
+    global calculation
+    calculation += str(symbol)
+    text_result.delete(1.0, "end")
+    text_result.insert(1.0,calculation)
+
+
+
+def evaluate_calculation():
+    global calculation
+    try:
+        calculation = str(eval(calculation))
+        calculation =""
+        text_result.delete(1.0, "end")
+        text_result.insert(1.0, calculation)
+        
+
+
+    except :
+        clear_field()
+        text_result.insert(1.0,"Error")
+
+def clear_field():
+    global calculation
+    calculation=""
+    text_result.delete(1.0,"end")
+
+root = tk.Tk()
+root.geometry("300x275")
+text_result = tk.Text(root , height=2 , width = 16 ,font=("C059" ,24))
+text_result.grid(columnspan=5)
+root.mainloop()
